75b3d5938e359f3afd78d205f63d88af9cfac6a0
David Naranjo <dnaranjo89@gmail.com>
2015-10-24 20:34:54 +0200

    Clean up

--- src/js/map_wrapper.js
+++ src/js/map_wrapper.js
@@ -7,46 +7,52 @@ AccesibleMap.setup = function(){
       attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | <a href="http://www.openstreetmap.org/about" target="_blank">&copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
     });
     layer.addTo(map);
-    //map.setView([41.8758,-87.6189], 16);
+
+    // Focus map on Caceres
+    map.setView([39.472499, -6.376273], 15);
     AccesibleMap.map = map;
 };
 
-AccesibleMap.draw_routes= function (){
-    var waypoints,
-        costing_options,
-        mode;
 
-    // Route 1
-    waypoints = [
-          L.latLng(39.474346, -6.375368),
-          L.latLng(39.473924, -6.378133)
-        ];
-    costing_options = {};
-    mode = "auto";
-    //calculate_route(waypoints, mode, costing_options);
-
-    // Route 2
-    waypoints = [
-          L.latLng(39.473684, -6.377592),
-          L.latLng(39.473949, -6.378597)
-        ];
-    costing_options = {
-        "pedestrian": {
-            "step_penalty": 5000
-        }
-    };
-    mode = "pedestrian";
-    calculate_route(waypoints, mode, costing_options);
+AccesibleMap.test_route = function(){
+    var origen = L.latLng(39.474346, -6.375368);
+    var parking = L.latLng(39.473684, -6.377592);
+    var destination = L.latLng(39.473949, -6.378597);
+    var step_penalty = 5000;
+    AccesibleMap.draw_complete_route(origen, parking, destination, step_penalty);
 };
 
-var calculate_route =  function (waypoints, mode, costing_options){
+AccesibleMap.draw_complete_route = function (origen, parking, destination, step_penalty){
+    AccesibleMap.calculate_route_auto(origen, parking).addTo(AccesibleMap.map);
+    AccesibleMap.calculate_route_pedestrian(parking, destination, step_penalty).addTo(AccesibleMap.map);
+};
+
+AccesibleMap.calculate_route_auto = function(origen, destination){
+    var waypoints = [origen, destination];
+    var costing_options = {};
+    var mode = "auto";
+    return AccesibleMap.calculate_route(waypoints, mode, costing_options);
+}
+
+AccesibleMap.calculate_route_pedestrian = function(origen, destination, step_penalty){
+    var waypoints = [origen, destination];
+    var costing_options = {
+        "pedestrian": {
+            "step_penalty": step_penalty
+        }
+    };
+    var mode = "pedestrian";
+    return AccesibleMap.calculate_route(waypoints, mode, costing_options);
+};
+
+AccesibleMap.calculate_route = function (waypoints, mode, costing_options){
     var styles = [
             {color: 'white',opacity: 0.8, weight: 12},
             {color: '#2676C6', opacity: 1, weight: 6}
         ];
     styles[1].color = (mode == "pedestrian") ? '#76c626' : '#2676C6';
 
-    L.Routing.control({
+    return L.Routing.control({
         waypoints: waypoints,
         lineOptions: {
             styles: styles
@@ -55,5 +61,5 @@ var calculate_route =  function (waypoints, mode, costing_options){
         formatter: new L.Routing.Valhalla.Formatter(),
         summaryTemplate:'<div class="start">{name}</div><div class="info {transitmode}">{distance}, {time}</div>',
         routeWhileDragging: false
-    }).addTo(AccesibleMap.map);
+    });
 };
