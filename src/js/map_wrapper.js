3a38a4a1045108bb79ee9a57693694f60eb34938
David Naranjo <dnaranjo89@gmail.com>
2015-10-25 13:44:04 +0100

    clean up

--- src/js/map_wrapper.js
+++ src/js/map_wrapper.js
@@ -1,12 +1,6 @@
-/**
- * https://mapzen.com/documentation/tangram/Scene-file/
- * Scenes: https://github.com/tangrams?query=demo
- * Simple: https://raw.githubusercontent.com/tangrams/nogui-demo/gh-pages/scene.yaml
- * Simple2: https://raw.githubusercontent.com/tangrams/simple-demo/gh-pages/scene.yaml
- */
-
 
 var AccesibleMap = {};
+
 AccesibleMap.setup2 = function(){
     var map = L.map('map');
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -19,39 +13,12 @@ AccesibleMap.setup2 = function(){
     AccesibleMap.map = map;
 };
 
-AccesibleMap.setup = function(){
-    var map = L.map('map');
-    var layer = Tangram.leafletLayer({
-        maxZoom: 20,
-      //scene: 'scenes/original.yaml', // Original
-      //scene: 'scenes/nogui-scene/scene.yaml', // Original
-      scene: 'scenes/eraser-map/eraser-map.yaml', // Best option
-      //scene: 'https://raw.githubusercontent.com/valhalla/valhalla-docs/master/examples/skin-and-bones-scene.yaml', // Original
-      //scene: 'https://raw.githubusercontent.com/tangrams/simple-demo/gh-pages/scene.yaml',  // Simple 1
-      //scene: 'https://raw.githubusercontent.com/tangrams/nogui-demo/gh-pages/scene.yaml',  // Simple
-      //scene: 'https://raw.githubusercontent.com/tangrams/eraser-map/gh-pages/eraser-map.yaml',  // Steps without street names
-      attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | <a href="http://www.openstreetmap.org/about" target="_blank">&copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
-    });
-    layer.addTo(map);
-
-    // Focus map on Caceres
-    /*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 20,
-        id: 'dnaranjo89.97a21b99',
-        accessToken: 'pk.eyJ1IjoiZG5hcmFuam84OSIsImEiOiJjaWc1Z2p4ZTc0MW0ydWttM3Mxem44cmVlIn0.qYwIDUVfbIQ2x2a9IQgg-g'
-    }).addTo(map);
-    map.setView([39.472499, -6.376273], 15);*/
-    AccesibleMap.map = map;
-};
-
-
 AccesibleMap.test_route = function(){
     var origen = L.latLng(39.474346, -6.375368);
     var parking = L.latLng(39.473684, -6.377592);
     var destination = L.latLng(39.473949, -6.378597);
-    var step_penalty = 5000;
-    AccesibleMap.draw_complete_route(origen, parking, destination, step_penalty);
+    var avoid_stairs = true;
+    AccesibleMap.draw_complete_route(origen, parking, destination, avoid_stairs);
 };
 
 AccesibleMap.draw_complete_route = function (origen, parking, destination, step_penalty){
@@ -66,13 +33,10 @@ AccesibleMap.calculate_route_auto = function(origen, destination){
     return AccesibleMap.calculate_route(waypoints, mode, costing_options);
 };
 
-AccesibleMap.calculate_route_pedestrian = function(origen, destination, step_penalty){
+AccesibleMap.calculate_route_pedestrian = function(origen, destination, avoid_stairs){
     var waypoints = [origen, destination];
-    var costing_options = {
-        "pedestrian": {
-            "step_penalty": step_penalty
-        }
-    };
+    var step_penalty = avoid_stairs ? 99999 : 0;
+    var costing_options = {"pedestrian": {"step_penalty": step_penalty }};
     var mode = "pedestrian";
     return AccesibleMap.calculate_route(waypoints, mode, costing_options);
 };
